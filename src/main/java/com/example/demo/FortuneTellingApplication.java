6d52f28d1eb2afb9d8d820d1f318d1c194a54dd9
3030955 <3030955@30309-55>
2024-05-24 18:07:43 +0900

    t

---
+++ src/main/java/com/example/demo/FortuneTellingApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortuneTellingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FortuneTellingApplication.class, args);
+	}
+
+}
